16bbdc782ebf2ce452fc83f596153fb6ddc4580b
hussain <bc250227101mhu@vu.edu.pk>
2026-05-01 22:18:09 +0500

    5th commit

--- playground/urls.py
+++ playground/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path 
-from debug_toolbar.toolbar import debug_toolbar_urls
 from . import views
 
 urlpatterns = [
     path('home/', views.hello_world,name='home')
-]+ debug_toolbar_urls()
+]
