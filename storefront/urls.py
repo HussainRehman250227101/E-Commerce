53ae5731a51a8c2cc38810a904b3c7d0745c218f
hussain <bc250227101mhu@vu.edu.pk>
2026-06-27 11:29:37 +0500

    testing

--- storefront/urls.py
+++ storefront/urls.py
@@ -3,10 +3,12 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 # from debug_toolbar.toolbar import debug_toolbar_urls
+from store.views import Ping
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ping/', Ping.as_view()),
     path('playground/', include('playground.urls')),
     path('store/', include('store.urls')),
     path('auth/', include('djoser.urls')),
